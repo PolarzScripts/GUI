5dc8624cf5dd0037b107b37d232c23b9923fbd5e
PolarzScripts <102315314+PolarzScripts@users.noreply.github.com>
2022-03-25 12:16:29 +0800

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,14 +1,102 @@
-local function CreateInstance(cls,props)
-local inst = Instance.new(cls)
-for i,v in pairs(props) do
-	inst[i] = v
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Main = Instance.new("ScreenGui")
+local MainFrame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Title = Instance.new("TextLabel")
+local Autoclick = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_4 = Instance.new("UICorner")
+
+--Properties:
+
+Main.Name = "Main"
+Main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = Main
+MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+MainFrame.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
+MainFrame.BorderSizePixel = 10
+MainFrame.Position = UDim2.new(0.499543786, 0, 0.498896241, 0)
+MainFrame.Size = UDim2.new(0, 471, 0, 300)
+
+UICorner.Parent = MainFrame
+
+TextButton.Parent = MainFrame
+TextButton.BackgroundColor3 = Color3.fromRGB(113, 113, 113)
+TextButton.Position = UDim2.new(0.0594479814, 0, 0.180000007, 0)
+TextButton.Size = UDim2.new(0, 151, 0, 52)
+TextButton.Font = Enum.Font.Gotham
+TextButton.Text = "Pet Simulator X Gamepasses"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+UICorner_2.Parent = TextButton
+
+Title.Name = "Title"
+Title.Parent = MainFrame
+Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title.BackgroundTransparency = 1.000
+Title.Size = UDim2.new(0, 471, 0, 50)
+Title.Font = Enum.Font.GothamBold
+Title.Text = "Polar's Scripts"
+Title.TextColor3 = Color3.fromRGB(0, 0, 0)
+Title.TextScaled = true
+Title.TextSize = 14.000
+Title.TextWrapped = true
+
+Autoclick.Name = "Autoclick"
+Autoclick.Parent = MainFrame
+Autoclick.BackgroundColor3 = Color3.fromRGB(113, 113, 113)
+Autoclick.Position = UDim2.new(0.575371563, 0, 0.180000007, 0)
+Autoclick.Size = UDim2.new(0, 151, 0, 52)
+Autoclick.Font = Enum.Font.Gotham
+Autoclick.Text = "Clicker Simulator Autoclick"
+Autoclick.TextColor3 = Color3.fromRGB(0, 0, 0)
+Autoclick.TextScaled = true
+Autoclick.TextSize = 14.000
+Autoclick.TextWrapped = true
+
+UICorner_3.Parent = Autoclick
+
+TextLabel.Parent = MainFrame
+TextLabel.BackgroundColor3 = Color3.fromRGB(117, 117, 117)
+TextLabel.Position = UDim2.new(0.00636942685, 0, 0.779999971, 0)
+TextLabel.Size = UDim2.new(0, 468, 0, 50)
+TextLabel.Font = Enum.Font.GothamBold
+TextLabel.Text = "More coming soon!"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+UICorner_4.Parent = TextLabel
+
+-- Scripts:
+
+local function PXNZQA_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	local gmppath = require(game.ReplicatedStorage.Framework.Modules.Client["5 | Gamepasses"])
+	gmppath.Owns = function() return true end
 end
-return inst
-end
+coroutine.wrap(PXNZQA_fake_script)()
+local function SDZAOI_fake_script() -- Autoclick.LocalScript 
+	local script = Instance.new('LocalScript', Autoclick)
+
 	
-local Main = CreateInstance('ScreenGui',{DisplayOrder=0,Enabled=true,ResetOnSpawn=true,Name='Main', Parent=game.CoreGui})
-local MainFrame = CreateInstance('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0.5, 0.5),BackgroundColor3=Color3.new(0.156863, 0.156863, 0.156863),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=10,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.480839401, 0, 0.568432689, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 350, 0, 273),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = 'MainFrame',Parent = Main})
-local PSXGamepass = CreateInstance('TextButton',{Font=Enum.Font.Nunito,FontSize=Enum.FontSize.Size14,Text='PSX All Gamepasses',TextColor3=Color3.new(0, 0, 0),TextScaled=true,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,AutoButtonColor=true,Modal=false,Selected=false,Style=Enum.ButtonStyle.Custom,Active=true,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.294118, 0.294118, 0.294118),BackgroundTransparency=0.5,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.0257142875, 0, 0.227106228, 0),Rotation=0,Selectable=true,Size=UDim2.new(0, 147, 0, 50),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='PSXGamepass',Parent = MainFrame})
-local ClickerSim = CreateInstance('TextButton',{Font=Enum.Font.RobotoMono,FontSize=Enum.FontSize.Size14,Text='Clicker Simulator Autoclick',TextColor3=Color3.new(0, 0, 0),TextScaled=true,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,AutoButtonColor=true,Modal=false,Selected=false,Style=Enum.ButtonStyle.Custom,Active=true,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(0.372549, 0.372549, 0.372549),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.537142873, 0, 0.227106228, 0),Rotation=0,Selectable=true,Size=UDim2.new(0, 151, 0, 50),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='ClickerSim',Parent = MainFrame})
-local Title = CreateInstance('TextLabel',{Font=Enum.Font.Jura,FontSize=Enum.FontSize.Size14,Text='Polar Hub',TextColor3=Color3.new(0, 0, 0),TextScaled=true,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, -0.0366300382, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 350, 0, 62),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='Title',Parent = MainFrame})
-local More = CreateInstance('TextLabel',{Font=Enum.Font.Highway,FontSize=Enum.FontSize.Size14,Text='More buttons coming soon!',TextColor3=Color3.new(0, 0, 0),TextScaled=true,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0, 0, 0.816849828, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 350, 0, 50),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='More',Parent = MainFrame})
+	
+	
+	game.ReplicatedStorage.Clickerr:InvokeServer()
+end
+coroutine.wrap(SDZAOI_fake_script)()
